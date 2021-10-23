be1e3e3d6c79e8b91158bf347521bd1a87190fe4
monikaprajapt <92983459+monikaprajapt@users.noreply.github.com>
2021-10-23 10:09:59 +0530

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,36 @@
+# atm=(input("*enter the atm*"))
+# card=(input("enter the card"))
+# if  card =="dabitcard" or "cridtcard":
+#     print("card is right")
+#     pin=(input("enter the pin number"))
+#     if pin=="4number"or"6number":
+#         print("correct")
+#         lanuage=input("selcet the lanuage")
+#         if lanuage=="English" or "hindi":
+#             print("correct")
+#             transction=input("selcet the transction")                                          
+#             if transction=="case withdrawal":
+#                 print("in procing")
+#                 type=input("selcet the amount")
+#                 if type=="seaving" or "correcent":
+#                     print("seaving")
+#                     amount=int(input("enter the amount"))
+#                     if amount>=500 and amount<=10000:
+#                         print("collect your amount")
+#                         receipt=(input("do you want receipt"))
+#                         if receipt=="yes"or"no":
+#                             print("transctio has been done sucffuly")
+#                         else:
+#                            print("not sucffuly")
+#                     else:
+#                          print("collect not amount")
+#                 else:
+#                     print("not saving")
+#             else:
+#                 print("in not procing")
+#         else:
+#             print("hindi language")
+#     else:
+#         print("not correct")
+# # else:
+# #     print("card is not right")
